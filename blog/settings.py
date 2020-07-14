04c6a8a67453b3ee4ed0d5628614f74176010e63
BirdOnTheBranch <planelles.luis@gmail.com>
2020-07-14 14:09:12 +0200

    Implemented new function to send mails and comments in the articles, and starting taggit sistem.

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#gmail
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'Planelles.luis@gmail.com'
+EMAIL_HOST_PASSWORD = 'Gus@r@p0'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
